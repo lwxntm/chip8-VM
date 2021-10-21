ec0ed50538f35cbae702fcb49a65b874ce0f9525
xiaotian <canxuetian@gmail.com>
2021-10-21 23:25:20 +0800

    添加程序load

--- src/com/github/lwxntm/emu/Main.java
+++ src/com/github/lwxntm/emu/Main.java
@@ -2,13 +2,34 @@ package com.github.lwxntm.emu;
 
 import com.github.lwxntm.chip.Chip;
 
-public class Main {
+public class Main extends Thread {
+    private Chip chip8;
+    private ChipFrame frame;
+
+    public Main() {
+        chip8 = new Chip();
+        chip8.init();
+        chip8.loadProgram("./pong.ch8");
+        frame = new ChipFrame(chip8);
+    }
+    public void run(){
+        //chip8的CPU频率是60Hz,也就是每秒更新60次
+        while (true){
+            chip8.run();
+            if (chip8.needRedraw()){
+                frame.repaint();
+                chip8.removeDrawFlag();
+            }
+            try {
+                Thread.sleep(16);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
     public static void main(String[] args) {
-        Chip c=new Chip();
-        c.init();
-     //   c.run();
-        ChipFrame f=new ChipFrame(c);
-
+        Main main = new Main();
     }
 }
